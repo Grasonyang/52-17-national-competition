ba4dd545c2202f78c32e97f220d562075b00bc10
Grasonyang <75509171+Grasonyang@users.noreply.github.com>
2022-12-22 11:49:14 +0800

    Add files via upload

---
+++ TeskA/02/TasKA02.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="TaskA02.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="loader">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+</body>
+
+</html>
