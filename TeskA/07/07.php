a6cf3b1c75be69d8d70c9c3f9634764f56292e4d
Grasonyang <75509171+Grasonyang@users.noreply.github.com>
2022-12-27 21:33:43 +0800

    Add files via upload

--- TeskA/07/07.php
+++ TeskA/07/07.php
@@ -1,10 +1,20 @@
 <?php
+if ($_GET['url'] != null || $_GET['url'] != '') {
+  $url = $_GET['url'];
+  if (strpos($url, '07.html') != false && $_SERVER['HTTP_REFERER'] != null) {
     $folder = 'images';
     $images = scandir($folder);
+    // if($_SERVER['PHP_SELF'])
+    // echo ;
     foreach ($images as $image) {
-        if ($image === '.' || $image === '..') {
-            continue;
-        }
-        echo '<img src="' . $folder . '/' . $image . '" alt="image">';
+      if ($image === '.' || $image === '..') {
+        continue;
+      }
+      // echo '<img src="' . $folder . '/' . $image . '">';
     }
-?>
+  } else {
+    echo "Status Code: 403";
+  }
+} else {
+  echo "Status Code: 403";
+}
