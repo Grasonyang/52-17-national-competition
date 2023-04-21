48bf6649b8f716f843bcb449f76fb47dc618fb5e
Grasonyang <75509171+Grasonyang@users.noreply.github.com>
2023-04-21 11:39:50 +0800

    Add files via upload

---
+++ TeskB/model/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="src/jquery-3.6.4.min.js"></script>
+    <script src="src/jquery-ui.js"></script>
+    <link rel="stylesheet" href="src/jquery-ui.css">
+    <link rel="stylesheet" href="src/header/header.css">
+    <link rel="stylesheet" href="src/all.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header-1">
+            <!-- <button class="button-1">首頁</button>
+            <button class="button-1">最新旅遊消息</button>
+            <button class="button-1">熱門旅遊景點</button>
+            <button class="button-1">交通資訊</button>
+            <button class="button-1">聯絡我們</button>
+            <div>
+                <form action="#">
+
+                </form>
+            </div> -->
+        </div>
+        <div class="header-2"></div>
+    </header>
+    <main>
+        <div class="main-1">
+            <div class="main-1-1"></div> <!-- 最新旅遊消息區 -->
+            <div class="main-1-2">
+                <div class="main-1-2-1"></div> <!-- 景點搜尋表單 -->
+                <div class="main-1-2-2"></div> <!-- 熱門旅遊景點 -->
+            </div>
+        </div>
+        <div class="main-2"> 
+            <div class="main-2-1"></div> <!-- 交通資訊 -->
+            <div class="main-2-2"></div> <!-- 聯絡表單 -->
+        </div>
+    </main>
+    <footer>
+        <span>Copyright © GWTN 2022 - All rights reserved</span>
+    </footer>
+</body>
+</html>
